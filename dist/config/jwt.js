c0126dd5e39767f53f0ac3a3f815c3dc28d06bed
Your Name <you@example.com>
2025-12-17 23:56:37 +0700

    chore: remove node_modules, dist, prisma from git tracking

--- dist/config/jwt.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.jwtExpiresIn = exports.jwtSecret = void 0;
-const env_1 = require("./env");
-exports.jwtSecret = env_1.JWT_SECRET;
-exports.jwtExpiresIn = '7d';
--- dist/modules/affiliate/affiliate.public.route.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const affiliate_controller_1 = require("./affiliate.controller");
-const router = (0, express_1.Router)();
-router.get('/:affiliateId', affiliate_controller_1.redirectAffiliate);
-exports.default = router;
