c81d4e09888c01774e022445acf84befde4203a6
Your Name <you@example.com>
2025-12-22 12:04:30 +0700

    upload

--- src/modules/post/post.public.route.ts
+++ src/modules/post/post.public.route.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express'
-import { getPostBySlug, getPostsByTopic } from './post.public.controller'
+import { getPostBySlug, getPostsByTopic, listPosts } from './post.public.controller'
 import { cache } from '../../middlewares/cache.middleware'
 import { CACHE_TTL } from '../../config/constants'
 
 const router = Router()
 
+router.get('/', listPosts)
 router.get('/:slug', cache(CACHE_TTL.POST_DETAIL), getPostBySlug)
 router.get('/topic/:topicSlug', cache(CACHE_TTL.TOPIC_LIST), getPostsByTopic)
 
